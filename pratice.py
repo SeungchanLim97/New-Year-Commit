4db814cf93d0203b37b0e7fa3c9f51f153b38e0b
Seungchan Lim <kimchilover97@gmail.com>
2022-01-01 23:31:25 +0900

    commit pratice

---
+++ pratice.py
@@ -0,0 +1,20 @@
+# 숫자형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+
+# 문자형
+print('풍선')
+print("나비")
+ 
+ # Boolean형
+print(5> 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not(5>10))
